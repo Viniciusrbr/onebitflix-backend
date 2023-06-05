a5fa9d4e83e0f948e7d6eb856167fc7b4eb0bbd4
Viniciusrbr <viniciusgoes159@gmail.com>
2023-06-05 17:28:30 -0300

    Configurando o Sequelize

---
+++ src/database/index.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from 'sequelize';
+
+export const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'onebitflix_development',
+    username: 'postgres',
+    password: '1234admin',
+    define: {
+        underscored: true // converte de snake_case para camelCase
+    }
+})
