6ef0e790b595e2641c98dcd18af979647febad72
Viniciusrbr <viniciusgoes159@gmail.com>
2023-06-07 19:29:01 -0300

    adminjs: adicionada tradução e dashboard personalizado

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,8 +3,10 @@ import AdminJSExpress from '@adminjs/express';
 import AdminJSSequelize from '@adminjs/sequelize';
 import { sequelize } from '../database';
 import { adminJsResources } from './resources/resource';
-import { User } from '../models';
-import bcrypt from 'bcrypt';
+import { locale } from './locale';
+import { dashboardOptions } from './dashboard';
+import { brandingOptions } from './branding';
+import { authtenticationOptions } from './authentication';
 
 AdminJS.registerAdapter(AdminJSSequelize);
 
@@ -12,45 +14,17 @@ export const adminJs = new AdminJS({
     databases: [sequelize],
     rootPath: '/admin',
     resources: adminJsResources,
-    branding: {
-        companyName: 'OneBitFlix',
-        logo: '/logoonebitflix.svg',
-        theme: {
-            colors: {
-                primary100: '#ff0043',
-                primary80: '#ff1a57',
-                primary60: '#ff3369',
-                primary40: '#ff4d7c',
-                primary20: '#ff668f',
-                grey100: '#151515',
-                grey80: '#333333',
-                grey60: '#4d4d4d',
-                grey40: '#666666',
-                grey20: '#dddddd',
-                filterBg: '#333333',
-                accent: '#151515',
-                hoverBg: '#151515',
-            }
-        }
+    branding: brandingOptions,
+    dashboard: dashboardOptions,
+    locale: locale,
+})
+
+export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(
+    adminJs,
+    authtenticationOptions,
+    null,
+    {
+        resave: false,
+        saveUninitialized: false
     }
-})
-
-export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(adminJs, {
-    authenticate: async (email, password) => {
-        const user = await User.findOne({ where: { email } })
-
-        if (user && user.role === 'admin') {
-            const matched = await bcrypt.compare(password, user.password)
-
-            if (matched) {
-                return user
-            }
-        }
-
-        return false
-    },
-    cookiePassword: 'senha-do-cookie'
-}, null, {
-    resave: false,
-    saveUninitialized: false
-})
+)
