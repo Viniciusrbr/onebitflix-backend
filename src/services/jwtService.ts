c5f83c9582c9557a4d02bab1267ae767a5b5829d
Viniciusrbr <viniciusgoes159@gmail.com>
2023-06-13 10:51:22 -0300

    middleware de autorização

--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -1,9 +1,13 @@
-import jwt from 'jsonwebtoken'  
+import jwt from 'jsonwebtoken'
 
 const secret = 'chave-jwt' // ainda vou mudar isso
 
 export const jwtService = {
     signToken: (payload: string | object | Buffer, expiration: string) => {
         return jwt.sign(payload, secret, { expiresIn: expiration })
+    },
+
+    verifyToken: (token: string, callbackfn: jwt.VerifyCallback) => {
+        jwt.verify(token, secret, callbackfn)
     }
 }
