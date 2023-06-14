467b90b357089683484a0ee53d5914a69f2c3f44
Viniciusrbr <viniciusgoes159@gmail.com>
2023-06-14 20:05:17 -0300

    atualizando o endpoint de cursos para saber se ele foi favoritado ou curtido

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -39,5 +39,17 @@ export const favoriteService = {
                 courseId
             }
         })
+    },
+
+    isFavorited: async (userId: number, courseId: number) => {
+        const favorite = await Favorite.findOne({
+            where: {
+                userId,
+                courseId
+            }
+        })
+
+        return favorite !== null
     }
+
 }
