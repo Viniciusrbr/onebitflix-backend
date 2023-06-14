950fc3acd6fdbe43f26ce20d0e7e899b1fa0559c
Viniciusrbr <viniciusgoes159@gmail.com>
2023-06-14 17:07:19 -0300

    adicionado endpoints de dar e remover like de um curso

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,6 +3,7 @@ import { Course } from './Course'
 import { Episode } from './Episode'
 import { User } from './User';
 import { Favorite } from './Favorite';
+import { Like } from './Like';
 
 Category.hasMany(Course, { as: 'courses' })
 
@@ -10,6 +11,7 @@ Course.belongsTo(Category)
 Course.belongsToMany(User, { through: Favorite })
 Course.hasMany(Episode, { as: 'episodes' })
 Course.hasMany(Favorite, { as: 'FavoritesUsers', foreignKey: 'course_id' })
+Course.belongsToMany(User, { through: Like })
 
 Episode.belongsTo(Course)
 
@@ -18,11 +20,13 @@ Favorite.belongsTo(User)
 
 User.belongsToMany(Course, { through: Favorite })
 User.hasMany(Favorite, { as: 'FavoritesCourses', foreignKey: 'user_id' })
+User.belongsToMany(Course, { through: Like })
 
 export {
     Course,
     Category,
     Episode,
     Favorite,
+    Like,
     User
 }
